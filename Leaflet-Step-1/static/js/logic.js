504ff3b59729e15db5fe317f77532a4e6214ee53
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-04-19 15:45:08 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,8 @@
 // creating the map
 
 var eMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
+    center: [39.8283, -98.5795],
+    zoom: 4
   });
   
   // adding the tile layer
@@ -10,9 +10,39 @@ var eMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.light",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1Ijoiam10cml0bGUiLCJhIjoiY2s4amM4aDhrMDFqYTNmcGZvN29xdGs5OCJ9.vhznuyA0w5Yc__sf_JKlew"
   }).addTo(eMap);
   
   // storing the earthquake data as a link
   var quakeLink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  
+
+// looping time! filling variables with the appopriate info from the json data taken from the reference link
+d3.json(quakeLink, function(data) {
+    var jsonData = data;
+    console.log(data.features)
+    data.features.forEach(item => {
+        var latitude = item.geometry.coordinates[1];
+        var longitude = item.geometry.coordinates[0];
+        var magnitude = item.properties.mag;
+        var place = item.properties.place;
+
+        // adding circles!
+        L.circle([latitude, longitude], {
+            color: colorPicker(magnitude),
+            fillColor: colorPicker(magnitude),
+            fillOpacity: .80,
+            radius: magnitude * 20000
+        }).bindPopup("<h3>Location: " + place + "</h3> <h3>Magnitude: " + magnitude + "</h3>").addTo(eMap);
+    });
+});
+
+// setting it up to pick colors based on the magnitude
+function colorPicker(d) {
+    return d > 5 ? '#ff584c':
+           d > 4 ? '#f4815b':
+           d > 3 ? '#e9995a':
+           d > 2 ? '#dfb25a':
+           d > 1 ? '#d4ca59':
+                   '#cae359';
+};
+
